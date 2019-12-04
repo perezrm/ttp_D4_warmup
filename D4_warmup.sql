31a8dd90faf5270be711eb9b9da685b71df635dc
Rose Mary Perez <rsmryperez@gmail.com>
2019-12-04 10:08:50 -0500

    1stUpload

---
+++ D4_warmup.sql
@@ -0,0 +1,26 @@
+/***********
+
+Good morning. Working on this.
+
+************/
+
+/*
+Good morning all, here's today's warmup. For this one, there is no dataset we're working with here,
+so you have to look at the structure of the two tables, and come up with the query "by hand"
+(write/type it out).
+HINTS:
+For this question, be sure to write the query to include users who made no comments in January 2019.
+Assume the the formate for the dates is "YYYY-MM-DD"
+Assume for this histogram, the "bin size" for comments is 1, ie users will be counted together only
+if they made the exact same number of comments in January 2019 (as opposed to a range of counts).
+Soooo... if there were three users with comment counts respectively of 8, 11, 8, the output should
+give a count of 2 for the value of 8 comments and 1 for a value of 11 comments.
+For inspiration and checking your syntax, you could try a similar query on databased you have
+installed (eg. dvd_rental, northwind), and then translate that to this problem.
+For further inspiration, you can look at the videos I sent out last night.
+FINALLY, put your work/answer (including psuedo-code if you'd like) in a github repo and send it to
+Sean and I.
+
+And if anyone feels super confident with their answer and still has extra some time, can you modify
+the query to have bin width's (ie user's with 10-12 comments, 13-15 comments, etc, are counted together (definitely not necessary for this morning, definitely extra challenging).
+*/
